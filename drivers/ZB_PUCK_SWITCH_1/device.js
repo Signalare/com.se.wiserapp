96cdae24ca7ea6c242f218fcbc8f34e44aa90f4f
Marcus Hermansson <marcus.hermansson@signalare.se>
2020-07-03 09:47:22 +0200

    Added ESLint, eslint fixes.

--- drivers/ZB_PUCK_SWITCH_1/device.js
+++ drivers/ZB_PUCK_SWITCH_1/device.js
@@ -4,32 +4,30 @@ const { ZigBeeDevice } = require('homey-meshdriver');
 
 class ZB_PUCK_SWITCH_1 extends ZigBeeDevice {
 
-	onMeshInit() {
-		
-		// Developer options
-		// this.printNode();
-		// this.enableDebug();
+  onMeshInit() {
+    // Developer options
+    // this.printNode();
+    // this.enableDebug();
 
-		// Register onoff capability
-		if (this.hasCapability('onoff')) {
-			this.registerCapability('onoff', 'genOnOff', { endpoint: 0 });
-			this.registerAttrReportListener('genOnOff', 'onOff', 1, 60, 1, this.onOffReport.bind(this), 0)
-				.catch(err => {
-					this.error(err);
-				});
-			this._attrReportListeners['2_genOnOff'] = this._attrReportListeners['2_genOnOff'] || {};
-			this._attrReportListeners['2_genOnOff']['onOff'] = this.onOffReport.bind(this);
-		}
+    // Register onoff capability
+    if (this.hasCapability('onoff')) {
+      this.registerCapability('onoff', 'genOnOff', { endpoint: 0 });
+      this.registerAttrReportListener('genOnOff', 'onOff', 1, 60, 1, this.onOffReport.bind(this), 0)
+        .catch(err => {
+          this.error(err);
+        });
+      this._attrReportListeners['2_genOnOff'] = this._attrReportListeners['2_genOnOff'] || {};
+      this._attrReportListeners['2_genOnOff']['onOff'] = this.onOffReport.bind(this);
+    }
 
-		this.log('Driver has been initied');
-		
-	}
+    this.log('Driver has been initied');
+  }
 
-	// onOffReport
-	onOffReport(value) {
-		const parsedValue = (value === 1);
-		this.setCapabilityValue('onoff', parsedValue);
-	}
+  // onOffReport
+  onOffReport(value) {
+    const parsedValue = (value === 1);
+    this.setCapabilityValue('onoff', parsedValue);
+  }
 
 }
 
