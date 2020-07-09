be553933e990908622dc34b1144f34c58b8c8456
Marcus Hermansson <marcus.hermansson@signalare.se>
2020-07-09 17:01:32 +0200

    Chnaged drivers to support SDK3, added driver ZB_NGPB_SWITCH_1

--- drivers/ZB_NHPB_SWITCH_1/device.js
+++ drivers/ZB_NHPB_SWITCH_1/device.js
@@ -2,10 +2,10 @@
 
 const WiserGenericDevice = require('../../lib/ZB_WiserGenericDevice');
 
-class ZB_PUCK_SWITCH_1 extends WiserGenericDevice {
+class ZB_NHPB_SWITCH_1 extends WiserGenericDevice {
 
   wiserEndpoint = 1;
 
 }
 
-module.exports = ZB_PUCK_SWITCH_1;
+module.exports = ZB_NHPB_SWITCH_1;
